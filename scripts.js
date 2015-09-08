2201c90b48dedc0af4f97c95f2011410b77021fc
Alejandro Pérez Martín <aleperez92@gmail.com>
2015-09-08 09:24:36 +0200

    Minor changes

--- scripts.js
+++ scripts.js
@@ -1,14 +1,37 @@
-
 var canvas = document.getElementById('canvas'),
     ctx = canvas.getContext('2d');
 
-// draw square
-ctx.fillRect(25, 25, 25, 25);
+/**
+ * Helper functions
+ * Source: http://jaketrent.com/post/addremove-classes-raw-javascript/
+ */
+function hasClass(ele, cls) {
+    return !!ele.className.match(new RegExp('(\\s|^)' + cls + '(\\s|$)'));
+}
 
-// draw lines
-ctx.beginPath();
-ctx.moveTo(100, 100);
-ctx.lineTo(250, 100);
-ctx.lineTo(75, 250);
-ctx.strokeStyle = "#ff0000";
-ctx.stroke();
+function addClass(ele, cls) {
+    if (!hasClass(ele, cls)) ele.className += " " + cls;
+}
+
+function removeClass(ele, cls) {
+    if (hasClass(ele, cls)) {
+        var reg = new RegExp('(\\s|^)' + cls + '(\\s|$)');
+        ele.className = ele.className.replace(reg, ' ');
+    }
+}
+
+document.getElementById('drop-area').addEventListener('dragover', function() {
+    addClass(this, 'over');
+});
+
+document.getElementById('drop-area').addEventListener('dragleave', function() {
+    removeClass(this, 'over');
+});
+
+document.getElementById('drop-area').addEventListener('drop', function() {
+    removeClass(this, 'over');
+});
+
+window.addEventListener('dragover', function(evt){
+    evt.preventDefault();
+}, true);
